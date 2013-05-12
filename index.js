2ac00f30016e42356ea40a5d0dc17627b387139c
Kyle Hargraves <pd@krh.me>
2013-05-12 10:20:41 -0500

    jsdoc; rm tj.defaults
    global state like that is just unnecessary. Just build a reviver and
    keep it around.

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 "use strict";
 
-var tj = module.exports = {
-  defaults: {
-    resolver: global,
-    loader:   'fromTypedJSON',
-    key:      '_type'
-  }
-};
+var tj = module.exports = {};
 
 /**
  * Tiny wrapper around `JSON.parse` which will always use a new reviver
  * with the given options.
  *
  * @param {String} json A valid JSON string.
- * @return The parsed JSON
+ * @param {Object?} options Options are passed directly to `tj.revive`.
+ * @return The deserialized value
  * @see reviver
  */
 tj.revive = function(json, options) {
@@ -21,38 +16,47 @@ tj.revive = function(json, options) {
 };
 
 /**
+ * Generate a `reviver` function suitable for use with  `JSON.parse`.
+ *
  * @param {Object?} options
+ * @param {String} options.key The key used to define type names
+ * @param {Object|function(typeName: String, typeKey: String, obj: Object)} options.resolver
+ *   A namespace object containing your types (which should have the `loader` function exposed),
+ *   or a function that will return the correct object on which to find the `loader` function for
+ *   a given type.
+ * @param {String|function(obj: Object, typeName: String, typeKey: String)} options.loader
+ *   How to deserialize the object. Either the function name to call on the `resolver`'s output,
+ *   or a function that will do all of the work (in which case, `resolver` is unused).
  * @return {function(key, value)} A JSON reviver function
  */
 tj.reviver = function(options) {
   options = options || {};
-  var resolver = options.resolver || tj.defaults.resolver,
-      loader   = options.loader   || tj.defaults.loader,
-      typeKey  = options.key      || tj.defaults.key;
+  var resolver = options.resolver || global,
+      loader   = options.loader   || 'fromTypedJSON',
+      typeKey  = options.key      || '_type';
 
-  return function(key, value) {
+  return function(key, obj) {
     var id, type;
 
-    if (!value || !value.hasOwnProperty(typeKey))
-      return value;
+    if (!obj || !(id = obj[typeKey]))
+      return obj;
 
-    id = value[typeKey];
     if (typeof loader === 'function') {
-      delete value[typeKey];
-      return loader(value, id, typeKey);
+      delete obj[typeKey];
+      return loader(obj, id, typeKey);
     }
 
     if (typeof resolver === 'function')
-      type = resolver(id, typeKey, value);
-    else if (resolver.hasOwnProperty(id))
+      type = resolver(id, typeKey, obj);
+    else if (resolver[id])
       type = resolver[id];
     else
-      return value;
+      return obj;
 
     if (typeof type[loader] !== 'function')
-      return value;
+      return obj;
 
-    delete value[typeKey];
-    return type[loader](value, id, typeKey);
+    delete obj[typeKey];
+    return type[loader](obj, id, typeKey);
   };
 };
